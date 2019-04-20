32b5c0a8ec716adcc1d528ba86a85e24ad191c28
kilyaborodeno <kilyaborodeno@gmail.com>
2019-04-20 21:33:33 +0300

    Fixing bugs

--- Piece.hpp
+++ Piece.hpp
@@ -2,8 +2,10 @@
 #define PIECE_HPP
 
 #include <iostream>
+#include <cassert>
 #include "mylib.hpp"
 #include "Rects.hpp"
+
 using namespace std;
 
 class Piece
@@ -38,44 +40,32 @@ public:
 
 	void choose_rect(Rects& rects)
 	{
-		if(wall_x)
+		bool can_be_rotated = wall_x || wall_y;
+
+		if(can_be_rotated) // if an object cannot lean on one wall, try leaning it on another wall
 		{
-			vector<int> rects_at_wall = rects.count_rects_at_wall_x(); // all rectangle numbers at wall_x
-			choose_at_wall(rects, rects_at_wall);
-		}
-		else if(wall_y)
-		{
-			vector<int> rects_at_wall = rects.count_rects_at_wall_y();// all rectangle numbers at wall_y
-			choose_at_wall(rects, rects_at_wall);
+			choose(rects);
+			if(!placed)
+			{
+				rotate();
+				choose(rects);
+			}
 		}
 		else
-		{
-			vector<int> proper_rects;
-			for(size_t i = 0; i < rects.num.size(); ++i) // creating an array with proper rectangles
-			{
-				if(rects.num[i].fits(width, length) && !rects.num[i].chosen)
-					proper_rects.push_back(i);
-			}
-			if(proper_rects.size()) // choosing one of proper rectangles
-            {
-                int choise = rand()%proper_rects.size();
-                current_rect = proper_rects[choise];
-				placed = true;
-            }
-            else
-                current_rect = -1; // if no proper rectangle choose the whole room
-		}
+			choose(rects);
 	}
 
     // when generating coordinates the rectangle always fits the room
 	void gen_coords(Rect r)
 	{
-        rotate_to_wall();
-		if(wall_y && width > r.x2 - r.x1) rotate(); // rotating the object to fit in by width and length
-		if(wall_x && length > r.y2 - r.y1) rotate();
+//      rotate_to_wall();
+//		if(wall_y && width > r.x2 - r.x1) rotate(); // rotating the object to fit in by width and length
+//		if(wall_x && length > r.y2 - r.y1) rotate();
 
+assert(r.x1 < 1000 && r.x1 >= 0 && r.y1 < 1000 && r.x2 < 1000 && r.y2 < 1000 && r.x2 >= 0 && r.y1 >= 0 && r.y2 >= 0);
 		int free_w = r.x2 - r.x1 - width; // calculating the coordinates range
 		int free_h = r.y2 - r.y1 - length;
+
 		if(wall_x)
 		{
 //			cout << "GenCoordsX" << endl << "\t" << r.y1  << endl << "\t" << r.y1+free_h << endl << "GenCoordsEnd" << endl;
@@ -96,6 +86,8 @@ public:
 			y = r.y1 + rand()%free_h;
 		else y = r.y1;
 		color = rand_color(150);
+
+assert(r.x1 < 1000 && r.y1 < 1000 && r.x2 < 1000 && r.y2 < 1000);
 	}
 
 	void rotate()
@@ -109,7 +101,6 @@ public:
 private:
 	void choose_at_wall(Rects& rects, vector<int> rects_at_wall)
 	{
-		
 		for(size_t i = 0; i < rects_at_wall.size(); ++i) // deleting inappropriate rectangle numbers
 		{
 			if(!rects.num[rects_at_wall[i]].fits(width, length) || rects.num[rects_at_wall[i]].chosen == true)
@@ -122,7 +113,76 @@ private:
 			placed = true;
 		}
 		else
-			current_rect = -1; // if no proper rectangle choose the whole room
+		{
+			placed = false;
+		}
+	}
+	
+	void choose(Rects& rects)
+	{
+		if(wall_x && wall_y)
+			choose_in_corner(rects);
+		else if(wall_x)
+		{
+			vector<int> rects_at_wall = rects.count_rects_at_wall_x(); // all rectangle numbers at wall_x
+			choose_at_wall(rects, rects_at_wall);
+		}
+		else if(wall_y)
+		{
+			vector<int> rects_at_wall = rects.count_rects_at_wall_y();// all rectangle numbers at wall_y
+			choose_at_wall(rects, rects_at_wall);
+		}
+		else
+		{
+			vector<int> proper_rects;
+			for(size_t i = 0; i < rects.num.size(); ++i) // creating an array with proper rectangles
+			{
+				if(rects.num[i].fits(width, length) && !rects.num[i].chosen)
+					proper_rects.push_back(i);
+			}
+			if(proper_rects.size()) // choosing one of proper rectangles
+            {
+                int choise = rand() % proper_rects.size();
+                current_rect = proper_rects[choise];
+				placed = true;
+            }
+            else
+                placed = false;
+		}
+	}
+
+	void choose_in_corner(Rects& rects)
+	{
+		vector<int> rects_in_corner(4);	
+		int w = 0, l = 0;
+
+		for(size_t i = 0; i < rects.num.size(); ++i) // calculating the width of the room
+			w = max(rects.num[i].x2, w);
+		for(size_t i = 0; i < rects.num.size(); ++i) // calculating the length of the room
+			l = max(rects.num[i].y2, l);
+
+		for(size_t i = 0; i < rects.num.size(); i++) // choosing rectangles in corners
+		{
+			if(rects.num[i].x1 == 0 && rects.num[i].y1 == 0)	rects_in_corner[0] = i;
+			if(rects.num[i].x2 == w && rects.num[i].y1 == 0)	rects_in_corner[1] = i;
+			if(rects.num[i].x2 == w && rects.num[i].y2 == l)	rects_in_corner[2] = i;
+			if(rects.num[i].x1 == 0 && rects.num[i].y2 == l)	rects_in_corner[3] = i;
+		}
+
+		for(size_t i = 0; i < rects_in_corner.size(); ++i) // deleting inappropriate rectangle numbers
+		{
+			if(!rects.num[rects_in_corner[i]].fits(width, length) || rects.num[rects_in_corner[i]].chosen == true)
+				rects_in_corner.erase(rects_in_corner.begin() + i);
+		}
+
+		if(rects_in_corner.size()) // choosing one of proper rectangles
+		{
+			int choise = rand() % rects_in_corner.size();
+			current_rect = rects_in_corner[choise];
+			placed = true;
+		}
+		else 
+			placed = false;
 	}
 };
 
@@ -132,6 +192,8 @@ ostream& operator<<(ostream& out, const Piece& p)
 	out << "length: " << p.length << endl;
 	out << "width: " << p.width << endl;
 	out << "height: " << p.height << endl;
+	out << "x: " << p.x << endl;
+	out << "y: " << p.y << endl;
 	return out;
 }
 #endif // PIECE_HPP
--- Rects.hpp
+++ Rects.hpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <cstdlib>
+#include <cassert>
 
 using namespace std;
 
@@ -44,8 +45,7 @@ public:
 		vector<int> rects_at_wall;
 		int w = 0;
 		for(size_t i = 0; i < num.size(); ++i) // calculating the width of the room
-			if(num[i].x2 > w)
-				w = num[i].x2;
+			w = max(num[i].x2, w);
 		for(size_t i = 0; i < num.size(); ++i) // calculating the number of rectangles leaning on wall_x
         {
 			if(num[i].x1 == 0 || num[i].x2 == w)
@@ -59,8 +59,7 @@ public:
 		vector<int> rects_at_wall;
 		int len = 0;
 		for(size_t i = 0; i < num.size(); ++i) // calculating the length of the room
-			if(num[i].y2 > len)
-				len = num[i].x2;
+			len = max(num[i].y2, len);
 		for(size_t i = 0; i < num.size(); ++i) // calculating the number of rectangles leaning on wall_y
         {
 			if(num[i].y1 == 0 || num[i].y2 == len)
@@ -82,8 +81,12 @@ public:
         int x2 = num[number].x2;
         int y2 = num[number].y2;
 
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
+
         int sides = sides_at_wall(x1, y1, x2, y2, X1, Y1, X2, Y2);
 
+for (auto i : num) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 		if(sides == 4)
 			return number;
 
@@ -100,16 +103,31 @@ public:
 				case 234: r[1].x1 = x1; r[1].y1 = y1; r[1].x2 = X1; r[1].y2 = y2; break;
 				case 134: r[1].x1 = x1; r[1].y1 = y1; r[1].x2 = x2; r[1].y2 = Y1; break;
 				case 124: r[1].x1 = X2; r[1].y1 = y1; r[1].x2 = x2; r[1].y2 = y2; break;
+				default: assert(false);
 			}
+for (auto i : r) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 			for(size_t i = 0; i < 2; ++i)   // adding two new rectangles to an array
+			{
                 num.push_back(r[i]);
+				assert(num.back().x1 < 1000 && num.back().x2 < 1000 && num.back().y1 < 1000 && num.back().y2 < 1000);
+			}
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
+for (auto i : num) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 			return num.size()-2;    // returning new current rectangle number to make it chosen
 		}
 
 		if(sides == 2)
 		{
-			Rect r[4];
+			int8_t n;
+			if(s == 12 || s == 23 || s == 34 || s == 14) 
+				n = 4;
+			else
+				n = 3;
+			Rect r[n];
 			r[0].x1 = X1; r[0].y1 = Y1; r[0].x2 = X2; r[0].y2 = Y2; r[0].chosen = true; // Calculating the coordinates of four new rectangles
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
 			switch(s)
 			{
 				case 12: r[1].x1 = x1; r[1].y1 = Y2; r[1].x2 = X2; r[1].y2 = y2;
@@ -128,16 +146,31 @@ public:
 						 r[2].x1 = X2; r[2].y1 = y1; r[2].x2 = x2; r[2].y2 = Y1;
 						 r[3].x1 = X2; r[3].y1 = Y1; r[3].x2 = x2; r[3].y2 = y2;
 				break;
+				case 24: r[1].x1 = x1; r[1].y1 = y1; r[1].x2 = X1; r[1].y2 = y2;
+						 r[2].x1 = X2; r[2].y1 = y1; r[2].x2 = x2; r[2].y2 = y2;
+				break;
+				case 13: r[1].x1 = x1; r[1].y1 = y1; r[1].x2 = x2; r[1].y2 = Y1;
+						 r[2].x1 = x1; r[2].y1 = Y2; r[2].x2 = x2; r[2].y2 = y2;
+				break;
+				default: assert(false);
 			}
+for (auto i : r) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 			for(size_t i = 0; i < 4; ++i)// adding four new rectangles to an array
+			{
                 num.push_back(r[i]);
-			return num.size()-4;// returning new current rectangle number to make a it chosen
+			}
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
+for (auto i : num) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
+			return num.size()-n;// returning new current rectangle number to make a it chosen
 		}
 
 		if(sides == 1)
 		{
 			Rect r[6];
 			r[0].x1 = X1; r[0].y1 = Y1; r[0].x2 = X2; r[0].y2 = Y2; r[0].chosen = true; // Calculating the coordinates of six new rectangles
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
 			switch(s)
 			{
 				case 1: r[1].x1 = x1; r[1].y1 = y1; r[1].x2 = X2; r[1].y2 = Y1;
@@ -164,14 +197,21 @@ public:
 						r[4].x1 = X2; r[4].y1 = y1; r[4].x2 = x2; r[4].y2 = Y1;
 						r[5].x1 = X2; r[5].y1 = Y1; r[5].x2 = x1; r[5].y2 = y1;
 				break;
+				default: assert(false);
 			}
+for (auto i : r) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 			for(size_t i = 0; i < 6; ++i) // adding six new rectangles to an array
                 num.push_back(r[i]);
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
+for (auto i : num) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 			return num.size()-6;// returning new current rectangle number to make it chosen
 		}
 
 		// sides == 0
 		Rect r[9];
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
 		r[0].x1 = X1; r[0].y1 = Y1; r[0].x2 = X2; r[0].y2 = Y2; r[0].chosen = true; // Calculating the coordinates of nine new rectangles
 		r[1].x1 = x1; r[1].y1 = y1; r[1].x2 = X1; r[1].y2 = Y1;
 		r[2].x1 = X1; r[2].y1 = y1; r[2].x2 = X2; r[2].y2 = Y1;
@@ -181,8 +221,12 @@ public:
 		r[6].x1 = X1; r[6].y1 = Y2; r[6].x2 = X2; r[6].y2 = y2;
 		r[7].x1 = x1; r[7].y1 = Y2; r[7].x2 = X1; r[7].y2 = y2;
 		r[8].x1 = x1; r[8].y1 = Y1; r[8].x2 = X1; r[8].y2 = Y2;
+for (auto i : r) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 		for(size_t i = 0; i < 9; ++i)
             num.push_back(r[i]);// adding nine new rectangles to an array
+assert(x1 < 1000 && x1 >= 0 && y1 < 1000 && x2 < 1000 && y2 < 1000 && x2 >= 0 && y1 >= 0 && y2 >= 0);
+assert(X1 < 1000 && X1 >= 0 && Y1 < 1000 && X2 < 1000 && Y2 < 1000 && X2 >= 0 && Y1 >= 0 && Y2 >= 0);
+for (auto i : num) assert(i.x1 < 1000 && i.x1 >= 0 && i.y1 < 1000 && i.x2 < 1000 && i.y2 < 1000 && i.x2 >= 0 && i.y1 >= 0 && i.y2 >= 0);
 		return num.size()-9;// returning new current rectangle number to make it chosen
 	}
 
@@ -193,7 +237,7 @@ public:
         {
             if(!num[i].chosen)
             {
-                for(size_t k = 1; k < num.size(); ++k) // Searching for rectangles near rectangle number i
+               for(size_t k = 1; k < num.size(); ++k) // Searching for rectangles near rectangle number i
                 {
                     if(i == k || num[k].chosen)
                         continue;
@@ -227,18 +271,19 @@ public:
             goes = merge();
         }while(goes);
 	}
-private:
+protected:
+	// Uniting the to rectangles
     bool unite(int i, int k, bool left)
     {
-		Rect n; // new
-        if(left)
+		Rect n;
+        if(left) // the rectangle is upper or lefter from the main 
         {
             n.x1 = num[k].x1;
             n.y1 = num[k].y1;
             n.x2 = num[i].x2;
             n.y2 = num[i].y2;
         }
-		else
+		else // the rectangle is lower or righter from the main 
         {
             n.x1 = num[i].x1;
             n.y1 = num[i].y1;
@@ -251,11 +296,11 @@ private:
         num.push_back(n);
         return true;
     }
-
 };
 
 Rects create(int w, int h)
 {
+assert(w >= 0 && w < 1000 && h >= 0 && h < 1000);
 	Rects rects;
     Rect r;
     r.x1 = 0;
